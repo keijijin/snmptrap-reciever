7367784be6761e0e48f605f4a69bade6a27bc7e8
Keiji Jin <keijijin@gmail.com>
2023-01-23 15:47:13 +0900

    First commit

--- src/main/java/com/sample/SnmpTrapRouteBuilder.java
+++ src/main/java/com/sample/SnmpTrapRouteBuilder.java
@@ -11,13 +11,18 @@ public class SnmpTrapRouteBuilder extends RouteBuilder{
         JacksonXMLDataFormat jacksonXMLDataFormat = new JacksonXMLDataFormat();
         jacksonXMLDataFormat.setUseList(true);
 
-        from("snmp:127.0.0.1:162?protocol=udp&type=TRAP")
+        String snmpVersion = "&snmpVersion=1";
+        // String oids = "&oids=1.3.6.1.2.1.1.3.0,1.3.6.1.2.1.25.3.2.1.5.1,1.3.6.1.2.1.25.3.5.1.1.1,1.3.6.1.2.1.43.5.1.1.11.1.";
+
+        from("snmp:192.168.0.15:162?protocol=udp&type=TRAP" + snmpVersion)
             .convertBodyTo(String.class)
-            .log("Original: ${body}")
+            // .log("Original: ${body}")
             .unmarshal(jacksonXMLDataFormat)
             .marshal().json(JsonLibrary.Jackson)
-            .process(new FactProcessor())
-            .log("Fact: ${body}")
-            .to("direct:makedecision");
+            .log("Body: ${body}")
+            // .process(new FactProcessor())
+            // // .log("Fact: ${body}")
+            // .to("direct:makedecision")
+            ;
     }
 }
