11f6517272a2589eeea08a1b853c60bbabdd1eb8
Keiji Jin <keijijin@gmail.com>
2023-01-27 11:10:24 +0900

    Use SnmpConstants

--- src/main/java/com/sample/SnmpTrapRouteBuilder.java
+++ src/main/java/com/sample/SnmpTrapRouteBuilder.java
@@ -1,11 +1,9 @@
 package com.sample;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.component.jacksonxml.JacksonXMLDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
-import org.snmp4j.PDU;
-import org.snmp4j.PDUv1;
+import org.snmp4j.mp.SnmpConstants;
 
 public class SnmpTrapRouteBuilder extends RouteBuilder{
 
@@ -14,10 +12,7 @@ public class SnmpTrapRouteBuilder extends RouteBuilder{
         JacksonXMLDataFormat jacksonXMLDataFormat = new JacksonXMLDataFormat();
         jacksonXMLDataFormat.setUseList(true);
 
-        String snmpVersion = "&snmpVersion=0";
-        // String oids = "&oids=1.3.6.1.2.1.1.3.0,1.3.6.1.2.1.25.3.2.1.5.1,1.3.6.1.2.1.25.3.5.1.1.1,1.3.6.1.2.1.43.5.1.1.11.1.";
-
-        from("snmp:192.168.0.15:162?protocol=udp&type=TRAP" + snmpVersion)
+        from("snmp:192.168.0.15:162?protocol=udp&type=TRAP&snmpVersion=" + SnmpConstants.version2c)
             // .process(new PduConverter())
             .convertBodyTo(String.class)
             .log("Original: ${body}")
